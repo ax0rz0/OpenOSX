a048bef4bf88b035f436a76b569d011412b7a443
Vali0004 <diorcheats.vali@gmail.com>
2026-07-21 18:10:24 -0400

    self-host: flex works

--- nix/pkgs/flex.nix
+++ nix/pkgs/flex.nix
@@ -33,7 +33,10 @@ stdenv.mkDerivation {
     # Do not add old gnulib malloc/realloc replacement sources to that host
     # binary; modern host compilers reject their K&R malloc declarations.
     perl -0pi -e 's/\@CROSS_TRUE\@am__append_1 = \\\n\@CROSS_TRUE\@\s+\.\.\/lib\/malloc\.c \\\n\@CROSS_TRUE\@\s+\.\.\/lib\/realloc\.c/\@CROSS_TRUE\@am__append_1 =/s' src/Makefile.in
-    perl -0pi -e 's/am__objects_3 = \.\.\/lib\/stage1flex-malloc\.\$\(OBJEXT\)\s+\\\n\s+\.\.\/lib\/stage1flex-realloc\.\$\(OBJEXT\)/am__objects_3 =/s' src/Makefile.in
+    perl -0pi -e 's/\@CROSS_TRUE\@am__objects_3 = \.\.\/lib\/stage1flex-malloc\.\$\(OBJEXT\) \\\n\@CROSS_TRUE\@\s+\.\.\/lib\/stage1flex-realloc\.\$\(OBJEXT\)/\@CROSS_TRUE\@am__objects_3 =/s' src/Makefile.in
+    perl -0pi -e 's/^\@AMDEP_TRUE\@\@am__include\@ \@am__quote\@\.\.\/lib\/\$\(DEPDIR\)\/stage1flex-(malloc|realloc)\.Po\@am__quote\@\n//mg' src/Makefile.in
+    substituteInPlace src/Makefile.in \
+      --replace-fail 'stage1flex_LDADD = ' 'stage1flex_LDADD = -lm'
   '';
 
   configurePhase = ''
